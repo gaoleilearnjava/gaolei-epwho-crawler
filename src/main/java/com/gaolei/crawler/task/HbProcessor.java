0c4331dc5d08290e7849a7b59c35b61d2cbe80c6
gaoleilearnjava <1763140594@qq.com>
2019-11-15 15:31:40 +0800

    正确版本第二版,种类列表,列表页,详情页的爬取没有任何问题,可以执行.

--- src/main/java/com/gaolei/crawler/task/HbProcessor.java
+++ src/main/java/com/gaolei/crawler/task/HbProcessor.java
@@ -44,9 +44,7 @@ public class HbProcessor implements PageProcessor {
     public void process(Page page) {
         //获取产品种类的大列表
         List<Selectable> nodes = page.getHtml().xpath("//body/div[@class='evli_pro']/div[@class='evli_pro_list1']").nodes();
-//        System.out.println("抓取分类成功");
         if (nodes.size() != 0) {
-//            System.out.println("正在爬取种类列表");
             //说明是种类列表页面
             for (Selectable node : nodes) {
                 List<Selectable> types = node.xpath("//dl/dt").nodes();
@@ -61,60 +59,11 @@ public class HbProcessor implements PageProcessor {
             //说明是产品列表页面或者是产品详情页面
             //判断是列表页还是产品详情页
             if (page.getUrl().toString().contains("chanpin")) {
-                //产品主列表页
-                Selectable productsNode = page.getHtml().xpath("//body//div[@class='wrapper']/div[@class='proBox1']");
-                if (productsNode != null) {
-                    //获取所有的产品列表
-                    List<Selectable> productDtlLists = productsNode.xpath("//ul/li").nodes();
-                    for (Selectable productDtl : productDtlLists) {
-                        //不是标王就加入爬取列表
-                        String type = productDtl.xpath("/li/@class").toString();
-                        if (type.equals("")) {
-                            //获取到产品详情链接
-                            String productDtlUrl = productDtl.xpath("//h3/a/@href").toString();
-                            //将产品详情链接加入到爬取队列
-                            page.addTargetRequest(productDtlUrl);
-                        }
-                    }
-                }
-                //视频产品列表页
-                Selectable videoProductsNode = page.getHtml().xpath("//body/div[@class='wrapper']/div[@class='proBoxRight']");
-                if (videoProductsNode != null) {
-                    List<Selectable> videoProductDtlLists = videoProductsNode.xpath("//div[@class='proBoxRight']/ul/li").nodes();
-                    for (Selectable videoProductDtl : videoProductDtlLists) {
-                        String videoProductDtlUrl = videoProductDtl.xpath("//h2/a/@href").toString();
-                        //将视频产品详情链接加入到爬取队列
-                        page.addTargetRequest(videoProductDtlUrl);
-                    }
-                }
-
-                //先获取共多少页
-                String pageString = page.getHtml().xpath("//span[@class='jump']/text()").nodes().get(0).toString();
-                int indexOfPage = pageString.indexOf("页");
-                String pageStr = pageString.substring(1, indexOfPage);
-                Integer pageNum = Integer.valueOf(pageStr);
-                //获取当前是多少页
-                String url = page.getUrl().toString();
-                //将下一页加入到爬取列表中
-                if (url.contains("_")) {
-                    String[] strs = url.split("_");
-                    String head = strs[0];
-                    String tail = strs[1];
-                    Integer nextPage = Integer.valueOf(tail.split("\\.")[0].substring(1)) + 1;
-                    System.out.println("下一页是" + nextPage);
-                    if (nextPage < pageNum) {
-                        page.addTargetRequest(head + "_p" + nextPage + ".html");
-                    }
-                } else {
-                    if (pageNum >= 3) {
-                        String[] strings = url.split("\\.");
-                        page.addTargetRequest(strings[0] + "." + strings[1] + "." + strings[2] + "_p2.html");
-                    }
-                }
+                //爬取产品列表页
+                processProductList(page);
             } else {
                 //爬取商品详情页
-                //判断是哪种商品详情页
-                if (page.getHtml().xpath("//*[@class='location']//a[1]/text()") != null) {
+                if (page.getHtml().xpath("//*[@class='location']//a[1]/text()").toString() != null) {
                     //第一种(常见类型)
                     processProductDtlType1(page);
                 } else {
@@ -126,13 +75,67 @@ public class HbProcessor implements PageProcessor {
     }
 
     /**
-     * 第二种详情页面
+     * 爬取产品列表页
      *
      * @param page
      */
-    private void processProductDtlType2(Page page) {
-        //todo 写一下具体逻辑
+    private void processProductList(Page page) {
+        //产品主列表页
+        Selectable productsNode = page.getHtml().xpath("//body//div[@class='wrapper']/div[@class='proBox1']");
+        if (productsNode != null) {
+            //获取所有的产品列表
+            List<Selectable> productDtlLists = productsNode.xpath("//ul/li").nodes();
+            for (Selectable productDtl : productDtlLists) {
+                //不是标王就加入爬取列表
+                String type = productDtl.xpath("/li/@class").toString();
+                if (type.equals("")) {
+                    //获取到产品详情链接
+                    String productDtlUrl = productDtl.xpath("//h3/a/@href").toString();
+                    //将产品详情链接加入到爬取队列
+                    page.addTargetRequest(productDtlUrl);
+                }
+            }
+        }
+        //视频产品列表页
+        Selectable videoProductsNode = page.getHtml().xpath("//body/div[@class='wrapper']/div[@class='proBoxRight']");
+        if (videoProductsNode != null) {
+            List<Selectable> videoProductDtlLists = videoProductsNode.xpath("//div[@class='proBoxRight']/ul/li").nodes();
+            for (Selectable videoProductDtl : videoProductDtlLists) {
+                String videoProductDtlUrl = videoProductDtl.xpath("//h2/a/@href").toString();
+                //将视频产品详情链接加入到爬取队列
+                page.addTargetRequest(videoProductDtlUrl);
+            }
+        }
+
+        //先获取共多少页
+        String pageString = page.getHtml().xpath("//span[@class='jump']/text()").nodes().get(0).toString();
+        int indexOfPage = pageString.indexOf("页");
+        String pageStr = pageString.substring(1, indexOfPage);
+        Integer pageNum = Integer.valueOf(pageStr);
+        //获取当前是多少页
+        String url = page.getUrl().toString();
+        //将下一页加入到爬取列表中
+        if (url.contains("_")) {
+            String[] strs = url.split("_");
+            String head = strs[0];
+            String tail = strs[1];
+            Integer nextPage = Integer.valueOf(tail.split("\\.")[0].substring(1)) + 1;
+            System.out.println("下一页是" + nextPage);
+            if (nextPage <= pageNum) {
+                String nextPageUrl = head + "_p" + nextPage + ".html";
+                System.out.println(nextPageUrl);
+                page.addTargetRequest(nextPageUrl);
+            }
+        } else {
+            if (pageNum >= 3) {
+                String[] strings = url.split("\\.");
+                String nextPageUrl = strings[0] + "." + strings[1] + "." + strings[2] + "_p2.html";
+                System.out.println(nextPageUrl);
+                page.addTargetRequest(nextPageUrl);
+            }
+        }
     }
+
 
     /**
      * 第一种详情页面
@@ -159,6 +162,8 @@ public class HbProcessor implements PageProcessor {
 
         //获取产品类别
         String productType = html.xpath("//*[@class='location']//a[3]/text()").toString();
+        if (productType == null)
+            productType = html.xpath("//*[@class='location']//a[2]/text()").toString();
         if (productType != null)
             product.setProduct_type(productType);
 
@@ -187,45 +192,79 @@ public class HbProcessor implements PageProcessor {
 
         //获取产品数据模块.
         Selectable dataNode = html.xpath("//div[@class='productRt']");
+        if (dataNode.toString() != null) {
+            //获取产品价格
+            String price = dataNode.xpath("//div[@class='price']//td[@class='sale']/text()").toString();
+            if (price != null)
+                product.setProduct_price(price);
 
-        //获取产品价格
-        String price = dataNode.xpath("//div[@class='price']//td[@class='sale']/text()").toString();
-        if (price != null)
-            product.setProduct_price(price);
+            //获取产品最小订货量
+            String minOrder = dataNode.xpath("//div[@class='price']//span[@class='jisuanProNum']/text()").toString();
+            if (minOrder != null)
+                product.setProduct_min_ordered(minOrder + "台");
 
-        //获取产品最小订货量
-        String minOrder = dataNode.xpath("//div[@class='price']//span[@class='jisuanProNum']/text()").toString();
-        if (minOrder != null)
-            product.setProduct_min_ordered(minOrder + "台");
-
-        List<Selectable> dataNods = dataNode.xpath("//div[@class='proDetail0']/ul/li").nodes();
-        for (Selectable dataNod : dataNods) {
-            String key = dataNod.xpath("//span/text()").toString();
-            String value = dataNod.xpath("//b/text()").toString();
-            if (key != null && value != null)
-                switch (key) {
+            List<Selectable> dataNods = dataNode.xpath("//div[@class='proDetail0']/ul/li").nodes();
+            for (Selectable dataNod : dataNods) {
+                String key = dataNod.xpath("//span/text()").toString();
+                String value = dataNod.xpath("//b/text()").toString();
+                if (key != null && value != null)
+                    switch (key) {
+                        //获取产品型号
+                        case "型号":
+                            product.setProduct_model(value);
+                            break;
+                        //获取产品品牌
+                        case "品牌":
+                            product.setProduct_brand(value);
+                            break;
+                        //获取厂商性质
+                        case "厂商性质":
+                            product.setCompany_type(value);
+                            break;
+                        //获取所在地
+                        case "所在地":
+                            product.setProduct_location(value);
+                            break;
+                    }
+            }
+            // 获取更新时间
+            String updateTime = dataNode.xpath("//div[@class='productBtn']/p[1]/b[1]/text()").toString();
+            product.setUpdate_time(updateTime);
+        } else {
+            //获取产品价格
+            String price = html.xpath("//div[@class='price']//strong/text()").toString();
+            if (price != null)
+                product.setProduct_price(price);
+            String parameters = html.xpath("//div[@class='parameter']/text()").toString();
+            if (parameters != null) {
+                String[] parameterList = parameters.split("\\s+");
+                if (parameterList.length == 7) {
                     //获取产品型号
-                    case "型号":
-                        product.setProduct_model(value);
-                        break;
-                    //获取产品品牌
-                    case "品牌":
-                        product.setProduct_brand(value);
-                        break;
+                    String productModel = parameterList[1];
+                    product.setProduct_model(productModel);
                     //获取厂商性质
-                    case "厂商性质":
-                        product.setCompany_type(value);
-                        break;
+                    String companyType = parameterList[2];
+                    product.setCompany_type(companyType);
                     //获取所在地
-                    case "所在地":
-                        product.setProduct_location(value);
-                        break;
+                    String location = parameterList[3];
+                    product.setProduct_location(location);
+                    //获取更新时间
+                    String updateTime = parameterList[4] +" "+ parameterList[5];
+                    product.setUpdate_time(updateTime);
+                } else if (parameterList.length == 6){
+                    //获取厂商性质
+                    String companyType = parameterList[1];
+                    product.setCompany_type(companyType);
+                    //获取所在地
+                    String location = parameterList[2];
+                    product.setProduct_location(location);
+                    //获取更新时间
+                    String updateTime = parameterList[3] +" "+ parameterList[4];
+                    product.setUpdate_time(updateTime);
                 }
+            }
         }
 
-        // 获取更新时间
-        String updateTime = dataNode.xpath("//div[@class='productBtn']/p[1]/b[1]/text()").toString();
-        product.setUpdate_time(updateTime);
 
         //获取产品简介
         String profile = html.xpath("//body/div[@class='main']/article[1]//div[@class='aboutUsText']").toString();
@@ -261,15 +300,28 @@ public class HbProcessor implements PageProcessor {
         page.putField("product", product);
     }
 
+
+    /**
+     * 第二种详情页面
+     *
+     * @param page
+     */
+    private void processProductDtlType2(Page page) {
+        //todo 写一下具体逻辑
+    }
+
+
     @Override
     public Site getSite() {
         return this.site;
     }
 
+
+    /**
+     * 正式任务执行方法
+     */
     //定时任务,开始爬虫,每小时执行一次
-    //也可以改成用网页来执行的,使用controller
-    //@Scheduled(cron = "0 0 * * * ?")
-    //测试使用,立马执行
+    @Scheduled(cron = "0 0 * * * ?")
     @Scheduled(initialDelay = 1000, fixedDelay = 1000 * 1000)
     public void process() {
         logger.info("环保在线-商城产品列表任务开始执行...");
@@ -282,10 +334,13 @@ public class HbProcessor implements PageProcessor {
                 .run();//启动
     }
 
+//    /**
+//     * 测试方法
+//     */
 //    @Scheduled(initialDelay = 1000, fixedDelay = 1000 * 1000)
 //    public void process() {
 //        logger.info("环保在线-商城产品列表任务开始执行...");
-//        String url = "http://www.hbzhan.com/chanpin-7692.html";
+//        String url = "http://www.hbzhan.com/chanpin-9077.html";
 //        Spider.create(new HbProcessor())
 //                .addUrl(url)//添加网址
 //                .setScheduler(new QueueScheduler().setDuplicateRemover(new BloomFilterDuplicateRemover(10 * 10000)))//添加布隆过滤器)//添加任务队列
